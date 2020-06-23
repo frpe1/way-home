ab0600ac14a1bf547fb528aeb1148e21d4a03905
Fredrik <fredrik.persson@yh.nackademin.se>
2020-06-23 20:25:02 +0200

    Add post processing and modifying camera
    Added cinematic camera component , make it smoother, added post processing to  add some bloom, depth of field and colorized quality, moving character.

---
+++ Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{    
+    [SerializeField] private float gravityScale;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    private CharacterController controller;
+    private Vector3 moveDirection;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yStore = moveDirection.y;
+        moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+        moveDirection = moveDirection.normalized * moveSpeed;
+        moveDirection.y = yStore;
+
+        if (controller.isGrounded) {
+            moveDirection.y = 0f;
+            if (Input.GetButtonDown("Jump")) {
+                moveDirection.y = jumpForce;
+            }
+        }
+
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+        controller.Move(moveDirection * Time.deltaTime);
+
+    }
+}
